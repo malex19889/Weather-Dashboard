d778a05bf45c8a4016cb988acb63d0bb46eb514b
malex19889 <malex19889@yahoo.com>
2020-08-25 23:43:52 -0500

    adding some styling and working on icon in current weather section

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-// $("#searchBtn").on("click", function () {
-//   event.preventDefault();
-//   var city = $("#search").val();
-//   console.log(city);
-//   var queryURL = "http://api.openweathermap.org/data/2.5/uvi/weather?q=" + city + "&units=imperial&APPID=5d8fc476fadf80408832e74b2c7ff757";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET",
-//   }).then(function (response) {
-//     console.log(response);
-//     var info = response
-//     $("#current-city").text(response.name)
-//     $("#current-temp").text("Temp: "+response.main.temp)
-//     $("#current-humid").text("Humidity: "+response.main.humidity)
-//     $("#current-wind").text("Wind Speed: "+response.wind.speed)
-//     $("#current-uv")
-//   });
-// });
+$("#searchBtn").on("click", function () {
+  event.preventDefault();
+  var city = $("#search").val();
+  console.log(city);
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=5d8fc476fadf80408832e74b2c7ff757";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var currIcon = $("<img>");
+    var currIconCode = response.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/wn/"+currIconCode+"@2x.png"
+    currIcon.attr("src",iconUrl)
+    $("#current-city").append(currIcon)
+    $("#current-city").text(response.name)
+    $("#current-temp").text("Temp: "+response.main.temp+"℉")
+    $("#current-humid").text("Humidity: "+response.main.humidity+"%")
+    $("#current-wind").text("Wind Speed: "+response.wind.speed)
+    $("#current-uv")
+  });
+});
 $("#searchBtn").on("click", function () {
   event.preventDefault();
   var city = $("#search").val();
@@ -26,9 +30,12 @@ $("#searchBtn").on("click", function () {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+    // remove items from forecast section
+    $("#forecast").empty()
+    // loop to render 5 day forecast cards
     for (var i = 0; i < response.list.length;) {
         console.log(response.list[i]);
-       // create 5 day forcast cards
+       // create 5 day forcast card elements
        var forecastCard = $("<div>");
        var date = $("<h5>");
        var icon = $("<img>");
@@ -40,8 +47,8 @@ $("#searchBtn").on("click", function () {
       forecastCard.addClass("card bg-primary text-white p-3");
       date.text(response.list[i].dt_txt)
       icon.attr("src",iconUrl)
-      temp.text(response.list[i].main.temp)
-      humidity.text(response.list[i].main.humidity)
+      temp.text("Temp: "+response.list[i].main.temp+"℉")
+      humidity.text("Humidity: "+response.list[i].main.humidity+"%")
       // append card elemnts to html
       $("#forecast").append(forecastCard)
       forecastCard.append(date)
